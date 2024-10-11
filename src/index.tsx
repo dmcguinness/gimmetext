ead3198bc166fc495ee146d9835821f5dc3556c5
dmcguinness <dom11590@gmail.com>
2024-10-11 14:25:02 -0600

    get initial package ready with gemini

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { gimmeSomeCopy } from './helpers/getCopy';
 
-const Test =(props)=> {
-  const {label} = props;
-  return (
-    <div>
-      <button>test</button>
-    </div>
-  );
+const APIS = {
+  GEMINI: "GEMINI",
+  // OPENAI: "OPENAI", // TODO - allow openai
 }
-export default Test;
+
+const defaultPrompt = "What is the response in LOTR when Aragorn says: Legolas, what do your elf eyes see?"
+
+export async function GimmeP ({
+  apiKey,
+  apiChoice,
+  prompt,
+  ...props
+}: {
+  apiKey: string,
+  apiChoice: string,
+  prompt: string,
+}) {
+  const { text, error } = await gimmeSomeCopy(
+    apiKey,
+    prompt || defaultPrompt,
+    apiChoice || APIS.GEMINI
+  )
+  return (
+    <p {...props}>{text}</p>
+  )
+}
