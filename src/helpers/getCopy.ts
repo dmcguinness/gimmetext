c03b7bc6853b72f4d6de7da2c6d824fe3744ddc3
dmcguinness <dom11590@gmail.com>
2024-10-17 16:05:10 -0600

    localstorage and list gen

--- src/helpers/getCopy.ts
+++ src/helpers/getCopy.ts
@@ -5,15 +5,19 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 //   const api = new ChatGPTAPI({ apiKey })
 // }
 
-const getGEMINICopy = async ({apiKey, prompt, systemInstruction}) => {
+const getModel = (apiKey, systemInstruction) => {
   const genAI = new GoogleGenerativeAI(apiKey)
-
   let modelParams = { model: "gemini-1.5-flash" }
   if (!!systemInstruction) {
     modelParams['systemInstruction'] = systemInstruction 
   }
-  
+
   const model = genAI.getGenerativeModel(modelParams);
+  return model
+}
+
+const getGEMINICopy = async ({apiKey, prompt, systemInstruction}) => {
+  const model = getModel(apiKey, systemInstruction)
   const result = await model.generateContent(prompt);
   return result.response.text()
 }
@@ -52,20 +56,26 @@ export async function gimmeSomeCopy({
 export async function gimmeAList({
   apiKey,
   apiChoice,
-  prompt,
+  prompts,
   systemInstruction,
   length,
-}, {
+}: {
   apiKey: string,
-  prompt: string,
+  prompts: Array<string>,
   apiChoice: string,
   systemInstruction?: string,
   length: number
 }) {
-  if (!apiKey) return { text: "", error: new Error(CONSTANTS.MISSING_API_KEY) }
+  if (!apiKey) return { results: [], error: new Error(CONSTANTS.MISSING_API_KEY) }
 
   if (apiChoice === "GEMINI") {
-  }
+    const results = await Promise.all(prompts.map(async (prompt) => {
+      const text = await getGEMINICopy({prompt, apiKey, systemInstruction})
+      return text
+    }));
+    // TODO - error handling
+    return { results, error: null }
+  } 
 
-  return { list: [], error: new Error(CONSTANTS.UNSUPPORTED_API) }
+  return { results: [], error: new Error(CONSTANTS.UNSUPPORTED_API) }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ const APIS = {
   GEMINI: "GEMINI",
   // OPENAI: "OPENAI", // TODO - allow openai
 }
+
 const OMIT_PROPS = [
   'apiKey',
   'apiChoice',
@@ -16,23 +17,59 @@ const OMIT_PROPS = [
 const defaultPrompt = "What is the response in LOTR when Aragorn says: Legolas, what do your elf eyes see?"
 const CHARACTERS_PER_TOKEN = 4
 
+function setLocalStorageKey (uuid: string, value: string) {
+  let error, saved
+  try {
+    localStorage.setItem(`gimme-text-${uuid}`, value)
+    saved = true
+  } catch (err) {
+    error = err
+    console.warn(`error setting localstorage key: `, err)
+    saved = false
+  } finally {
+    return { error, saved }
+  }
+}
+
+function getLocalStorageKey(uuid: string) {
+  let error, value;
+  try {
+    value = localStorage.getItem(`gimme-text-${uuid}`);
+  } catch (err) {
+    error = err;
+    value = null;
+  } finally {
+    return { error, value };
+  }
+}
+
 const getText = async ({
   apiKey,
   apiChoice,
   prompt,
   context,
+  uuid,
 }: {
   apiKey: string,
   apiChoice?: string,
   prompt: string,
   context?: string,
+  uuid: string,
 }) => {
+  if (!!uuid) {
+    const { error, value } = getLocalStorageKey(uuid)
+    if (!!error) console.warn(`Error retrieving localstorage: ${error.message}`)
+    if (!error && value?.length) return { text: value, error: null }
+  }
+
   const { text, error } = await gimmeSomeCopy({
     apiKey,
     prompt: prompt || defaultPrompt,
     apiChoice: apiChoice || APIS.GEMINI,
     systemInstruction: context,
   })
+  
+  !!uuid && text?.length && setLocalStorageKey(uuid, text)
   return { text, error }
 }
 
@@ -109,6 +146,5 @@ export async function GimmeBlockQuote (props) {
 
 // TODO - generate lists <ol> and <ul> based on a prompt
 // TODO - generate a data table based on a prompt
-
 // TODO - generate code in <code></code> tag with gemini code prompt
 // TODO - cache info in localstorage
